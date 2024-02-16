c5c0b295a2409f1a67ee4558e6f654da63c2bcb3
Juan Garavito <jgaravitoh@unal.edu.co>
2024-02-16 13:26:28 -0500

    Stable and working history and dialogue system

--- Assets/Dialogues/Dialogue.cs
+++ Assets/Dialogues/Dialogue.cs
@@ -10,4 +10,6 @@ public class Dialogue : ScriptableObject
     public Image CharacterImage;
     public Dialogue nextDialogue, previousDialogue;
     public float waitTime = 0.02f;
+    public Color nameColor = new Color(1,1,1,1);
+    public Color textColor = new Color(1,1,1,1);
 }
